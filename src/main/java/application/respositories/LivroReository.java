b9ea23c068a571811240f03c6a365bb9845a360d
lucymara <luci.carla@live.com>
2021-09-28 18:51:26 +0000

    listagem

--- src/main/java/application/respositories/LivroReository.java
+++
@@ -1,8 +0,0 @@
-package application.respositories;
-import org.springframework.data.repository.CrudRepository;
-
-import application.models.Livro;
-public interface LivroReository extends 
- CrudRepository<Livro, Integer> {
-    
-}
